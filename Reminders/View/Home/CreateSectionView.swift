183546deda4095c4a7d74956a286cf364664cbeb
Yeonah Shim <simya0528@naver.com>
2025-02-04 01:12:05 +0900

    View Start

---
+++ Reminders/View/Home/CreateSectionView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateSectionView.swift
+//  Reminders
+//
+//  Created by 심연아 on 2/4/25.
+//
+
+import SwiftUI
+
+struct CreateSectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateSectionView()
+}
---
+++ Reminders/View/Home/ListCardView.swift
@@ -0,0 +1,19 @@
+//
+//  ListCardView.swift
+//  Reminders
+//
+//  Created by 심연아 on 2/4/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ListCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListCardView()
+}
